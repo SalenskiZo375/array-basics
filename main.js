c20e8c7d6b5fc4fb48887d97520be75a84822070
Zoey <153301664+SalenskiZo375@users.noreply.github.com>
2024-04-29 11:11:20 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+// Zoey Salenski
+// 29 april 2024
+// Project
+
+const firstName = "Zoey";
+
+console. log(`Hello ${firstName}!`)
+
+
+console.log('\n');
+console.log('\n');
+
+
+const countries = ["ireland", "france", "norway"];
+// countries.length = 3;
+console.log(`Number of elements in the countries array is: ${countries.length}`);
+
+
+
+console.log(`list of countries: ${countries}
+number of countries: ${countries.length}`);
+
+// const countrie = ["spain"];
+countries.unshift("spain");
+console.log(`adding spain using the unshift method: ${countries}`);
+
+
+
+
+
+console.log('\n');
+console.log('\n');
+
+
+const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+
+// console.log(plants.pop());
+
+
+console.log(plants);
+
+
+plants.pop();
+
+console.log(plants);
+
+
+
+
+
+
+
+
+
+
+
